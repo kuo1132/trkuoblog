cfe843d24d6cdb57feea15e07800d00e77b822f8
KUAN TING <kuo113225@gmail.com>
2025-10-26 02:36:50 -0600

    Merged existing code from https://trkuo.blog

---
+++ wp-content/plugins/bluehost-wordpress-plugin/build/4.7.0/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-url'), 'version' => 'b8153e507af634890782');
---
+++ wp-content/plugins/bluehost-wordpress-plugin/vendor/newfold-labs/wp-module-ecommerce/build/store-info/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '9b0e77902ff7b8400ea2');
